50789ca14a5be11a35bdda77ff4430a1e871f679
Samuel Ohrenberg <samuelohrenberg@gmail.com>
2024-10-15 02:38:27 -0500

    Implemented an error handler

---
+++ api/API/TheBattleLibrary.API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using TheBattleLibrary.API.Models;
+
+namespace TheBattleLibrary.API.Middlewares;
+
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            // Log the exception
+            _logger.LogError(ex, $"Unhandled exception occurred while processing the request: {ex.Message}");
+
+            // Handle the exception and return custom error response
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var errorResponse = new Error(exception.Message);
+
+        // Serialize the response and write it to the HTTP response stream
+        return context.Response.WriteAsJsonAsync(errorResponse);
+    }
+}
