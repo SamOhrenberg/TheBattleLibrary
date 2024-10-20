a1e9acaf34f15361a443e592de13c3fe7a105a8a
Samuel Ohrenberg <samuelohrenberg@gmail.com>
2024-10-19 20:27:04 -0500

    Plugged in the SqLite Startup

--- api/API/TheBattleLibrary.API/Program.cs
+++ api/API/TheBattleLibrary.API/Program.cs
@@ -13,6 +13,8 @@ namespace TheBattleLibrary.API
 
             // Add services to the container.
 
+            Data.Startup.ConfigureServices(builder.Services, builder.Configuration);
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
