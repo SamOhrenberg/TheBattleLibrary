fed308bcd29282ce2a0b5d99668ece7506cd4a07
Samuel Ohrenberg <samuelohrenberg@gmail.com>
2024-10-19 21:27:44 -0500

    Removed the Salt
    PasswordHasher handles salting for us

--- api/Data/TheBattleLibrary.Data.Entities/UserAccount.cs
+++ api/Data/TheBattleLibrary.Data.Entities/UserAccount.cs
@@ -16,18 +16,14 @@ public class UserAccount
     [MaxLength(255)]
     public string PasswordHash { get; set; }
 
-    [MaxLength(255)]
-    public string Salt { get; set; }
-
-    public UserAccount(string username, string passwordHash, string salt) : this(Guid.NewGuid(), username, passwordHash, salt)
+    public UserAccount(string username, string passwordHash) : this(Guid.NewGuid(), username, passwordHash)
     {
     }
 
-    public UserAccount(Guid id, string username, string passwordHash, string salt)
+    public UserAccount(Guid id, string username, string passwordHash)
     {
         Id = id;
         Username = username;
         PasswordHash = passwordHash;
-        Salt = salt;
     }
 }
