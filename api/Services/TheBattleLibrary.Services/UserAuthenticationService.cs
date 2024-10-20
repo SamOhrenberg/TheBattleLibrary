e8ff120d36ebac2cdbd04b08b5aa3f3c5161d52f
Samuel Ohrenberg <samuelohrenberg@gmail.com>
2024-10-19 21:37:58 -0500

    implemented user auth service

---
+++ api/Services/TheBattleLibrary.Services/UserAuthenticationService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using TheBattleLibrary.Data.Entities;
+
+namespace TheBattleLibrary.Services;
+
+public class UserAuthenticationService
+{
+    private readonly PasswordHasher _passwordHasher;
+
+    public UserAuthenticationService()
+    {
+        _passwordHasher = new PasswordHasher();
+    }
+
+    public UserAccount RegisterUser(string username, string password)
+    {
+        // Hash the password
+        string passwordHash = _passwordHasher.HashPassword(password);
+
+        var user = new UserAccount(username, passwordHash);
+
+        return user;
+    }
+
+    public bool ValidateUser(string hashedPassword, string password)
+    {
+        // Verify password
+        var verificationResult = _passwordHasher.VerifyHashedPassword(hashedPassword, password);
+
+        return verificationResult == PasswordVerificationResult.Success;
+    }
+}
