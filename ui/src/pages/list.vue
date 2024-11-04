c7f2138eba7a494872e088bc0cd42eeb9ec4b1e6
Samuel Ohrenberg <samuelohrenberg@gmail.com>
2024-11-04 15:22:41 -0600

    Update list.vue
    Added basic selection interface

--- ui/src/pages/list.vue
+++ ui/src/pages/list.vue
@@ -45,6 +45,31 @@
           </v-btn>
         </v-col>
       </v-row>
+
+      <!-- Form to add a new selection -->
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field v-model="newSelection.name" label="Selection Name" />
+          <v-text-field v-model="newSelection.type" label="Selection Type" />
+          <v-text-field v-model="newSelection.quantity" label="Quantity" type="number" />
+          <v-btn @click="addSelection">Add Selection</v-btn>
+        </v-col>
+      </v-row>
+
+      <!-- Display the list of selections -->
+      <v-row>
+        <v-col cols="12" md="4">
+          <h3>Selections</h3>
+          <v-list>
+            <v-list-item v-for="selection in listItem.selections" :key="selection.id">
+              <v-list-item-title>{{ selection.name }} ({{ selection.type }}) - {{ selection.quantity }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-col>
+      </v-row>
     </v-container>
   </v-form>
 </template>
@@ -54,6 +79,7 @@ import { useRoute, useRouter } from 'vue-router';
 import { useListStore } from '@/stores/list';
 import { reactive, computed } from 'vue';
 import { storeToRefs } from 'pinia';
+import {newGuid} from "@/utilities/guid";
 
 const router = useRouter();
 const route = useRoute();
@@ -70,6 +96,22 @@ const valid = computed(() => {
   return listItem.name !== ''
     && listItem.faction !== '';
 });
+
+const newSelection = reactive({
+  id: '',
+  name: '',
+  type: '',
+  quantity: 1,
+  selections: []
+});
+
+const addSelection = () => {
+  newSelection.id = newGuid();
+  listItem.selections.push({ ...newSelection });
+  newSelection.name = '';
+  newSelection.type = '';
+  newSelection.quantity = 1;
+};
 
 const saveButtonClick = () => {
   if (valid.value) {
